da19b9fd1d50d55d42d0e05d80fa4dbaf261cc50
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2023-01-01 15:18:24 +0530

    Complete Code for downloading anthem audio files

--- national_anthem_scrape/national_anthem.py
+++ national_anthem_scrape/national_anthem.py
@@ -3,18 +3,25 @@ from selenium.webdriver.common.by import By
 from tqdm import tqdm
 import os
 import national_anthem_scrape.constants as constants
-from datetime import datetime
+import time
+import pandas as pd
 
 class NationalAnthem(webdriver.Chrome):
-    def __init__(self, driver_path=r'C:/Users/Pratik Sanghavi/Desktop/Drivers/ChromeDriver', teardown=False):
+    def __init__(self,
+                 driver_path=r'C:/Users/Pratik Sanghavi/Desktop/Drivers/ChromeDriver',
+                 teardown=False,
+                 download_directory = "C:\\Users\\Pratik Sanghavi\\Desktop\\Projects\\National_Anthem\\national_anthem_scrape\\national_anthem_dataset\\audio_files"
+                 ):
         self.driver_path = driver_path
         self.teardown = teardown
-        self.links = None
+        self.download_directory = download_directory
         os.environ["PATH"] = driver_path
         options = webdriver.ChromeOptions()
-        prefs = {"download.default_directory": "C:/Users/Pratik Sanghavi/Desktop/Projects/National_Anthem_Dataset/national_anthem_scrape/national_anthem_dataset"}
+        prefs = {"download.default_directory": download_directory, "download.directory_upgrade": True, "download.prompt_for_download": False, "safebrowsing.enabled":True}
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_experimental_option("prefs", prefs)
+        options.add_argument("--headless")
+        # options.add_experimental_option("detach", True)
         super(NationalAnthem, self).__init__(options=options)
         self.implicitly_wait(10)
         self.maximize_window()
@@ -36,4 +43,26 @@ class NationalAnthem(webdriver.Chrome):
             for link_list in sub_menu.find_elements(By.TAG_NAME,'li'):
                 for link in link_list.find_elements(By.TAG_NAME, 'a'):
                     links.append(link.get_attribute('href'))
-        
+        return list(set(links))
+
+    def download_music(self, links):
+        names = []
+        mp3_files = []
+        for link in tqdm(links):
+            self.get(link)
+            try:
+                music_link = self.find_element(By.CSS_SELECTOR, 'a[id^="mp3"]')
+                title = self.find_element(By.TAG_NAME, 'h1')
+                music_link.click()
+                names.append(title.get_attribute('innerHTML'))
+                mp3_files.append(music_link.get_attribute('href').split('/')[-1])
+                time.sleep(5)
+            except:
+                print(f"Not present for: {link}")
+        key_df = pd.DataFrame({
+            'Country': names,
+            'Audio_File': mp3_files
+        })
+        key_df['File_Location'] = self.download_directory.replace("\\", "/") + "/" + key_df["Audio_File"]
+        key_location = self.download_directory.replace("\\", "/").replace("audio_files", "")
+        key_df.to_csv(f'{key_location}/key.csv', index = False)
--- run.py
+++ run.py
@@ -1,11 +1,9 @@
-import pandas as pd
-from tqdm import tqdm
-import os
 from national_anthem_scrape.national_anthem import NationalAnthem
 
 try:
     with NationalAnthem() as bot:
         bot.land_first_page()
-        bot.get_menu_link()
+        links = bot.get_menu_link()
+        bot.download_music(links)
 except Exception as e:
     print(f"Exception: \n{e}\nencountered! Please fix before rerunning.")
