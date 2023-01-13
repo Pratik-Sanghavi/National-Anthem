5326c0192baea401b97ca3b6141faed193db4376
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2023-01-13 16:45:24 +0530

    Initial World Plot Code added

--- k_means_optimal_clusters.py
+++ k_means_optimal_clusters.py
@@ -2,6 +2,7 @@ import torch
 import pandas as pd
 import numpy as np
 from kmeans_pytorch import kmeans
+from kneed import KneeLocator
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
@@ -13,11 +14,10 @@ start = 5
 stop = 30
 min_dist_list = []
 np.random.seed(42)
+X = pd.read_csv('national_anthem_scrape/national_anthem_dataset/features.csv')
+x = X.select_dtypes('number').values
+x = torch.from_numpy(x)
 for num_clusters in tqdm(range(start, stop)):
-    X = pd.read_csv('national_anthem_scrape/national_anthem_dataset/features.csv')
-    x = X.select_dtypes('number').values
-    x = torch.from_numpy(x)
-
     # kmeans
     cluster_ids_x, cluster_centers = kmeans(
         X=x, num_clusters=num_clusters, distance='euclidean', device=torch.device('cuda:0'), tqdm_flag = False
@@ -31,7 +31,13 @@ for num_clusters in tqdm(range(start, stop)):
         dist_list.append(dist)
     min_dist_list.append(sum(dist_list))
 
+kn = KneeLocator(np.arange(start, stop), min_dist_list, curve = 'convex', direction = 'decreasing')
+
 plt.plot(np.arange(start, stop), min_dist_list)
 plt.xticks(np.arange(start, stop,1))
+plt.vlines(kn.knee, plt.ylim()[0], plt.ylim()[1], linestyles='dashed')
+plt.xlabel('number of clusters k')
+plt.ylabel('Sum of squared distances')
 plt.grid()
+plt.show()
 plt.savefig('./k_means_clustering/artifacts/distance_metric.png', bbox_inches='tight')
