6d6dbdfa051599dbc62da619455e3321f15b0d08
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2023-01-14 17:35:29 +0530

    Written to file

--- feature_generation.py
+++ feature_generation.py
@@ -4,4 +4,4 @@ DATA_PATH = 'national_anthem_scrape/national_anthem_dataset/audio_files'
 OUTFILE = 'national_anthem_scrape/national_anthem_dataset/features.csv'
 
 aud_features = AudioFeatureGeneration(data_path=DATA_PATH, out_file=OUTFILE)
-print(aud_features.getDataset())
+aud_features.writeFile()
